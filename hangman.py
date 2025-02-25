521165312f58079337f180e88f74011f23def79c
Raeyy23 <yarendeveci477@gmail.com>
2025-02-25 22:55:15 +0300

    Version 2

--- hangman.py
+++ hangman.py
@@ -1,64 +1,64 @@
 import random
-print( ''')                                             
-| |                                            
-| |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
-| '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
-| | | | (_| | | | | (_| | | | | | | (_| | | | |
-|_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
-                    __/ |                      
-                    |___/                       
-''')
+#print( ''')                                             
+# | |                                            
+# | |__   __ _ _ __   __ _ _ __ ___   __ _ _ __  
+# | '_ \ / _` | '_ \ / _` | '_ ` _ \ / _` | '_ \ 
+# | | | | (_| | | | | (_| | | | | | | (_| | | | |
+# |_| |_|\__,_|_| |_|\__, |_| |_| |_|\__,_|_| |_|
+#                     __/ |                      
+#                     |___/                       
+# ''')
 HANGMANPICS = ['''
-    +---+
-    |   |
++---+
+|   |
+    |
+    |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+    |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+|   |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+/|  |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+/|\ |
+    |
+    |
+=========''', '''
++---+
+|   |
+O   |
+/|\ |
+/   |
+    |
+=========''', 
+'''
++----+
+|    |
+O    |
+/|\  |
+/ \  |
         |
-        |
-        |
-        |
-    =========''', '''
-    +---+
-    |   |
-    O   |
-        |
-        |
-        |
-    =========''', '''
-    +---+
-    |   |
-    O   |
-    |   |
-        |
-        |
-    =========''', '''
-    +---+
-    |   |
-    O   |
-    /|  |
-        |
-        |
-    =========''', '''
-    +---+
-    |   |
-    O   |
-    /|\ |
-        |
-        |
-    =========''', '''
-    +---+
-    |   |
-    O   |
-    /|\ |
-    /   |
-        |
-    =========''', 
-    '''
-    +----+
-    |    |
-    O    |
-    /|\  |
-    / \  |
-         |
-    =========''']
+=========''']
 
 #Word bank of animals
 words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
@@ -69,65 +69,64 @@ words = ('ant baboon badger bat bear beaver camel cat clam cobra cougar '
         'stork swan tiger toad trout turkey turtle weasel whale wolf '
         'wombat zebra ').split()
 
-d_list = ["camel", "diary", "smell", "car"]
+d_list = ["car"]
 lives = 0
 
 answer = random.choice(d_list)
 answerList = list(answer)
 print(answerList)
 blanks = len(answer)
-guessList =["_"] * blanks
 
+guessList =["_"] * blanks
 print(guessList)
 print(f"Word to guess: {"_ " * blanks}")
 guessLetter = input("Guess a letter: ")
 
-
+LetterList = [guessLetter]
 
 
 def letterCheck(let):
-        global lives, blanks
-        if let  in answerList and  let not in guessList:        
-            
-            while let   in answerList:
-                guessList[answerList.index(let)] == let
-                answerList.pop(answerList.index(let))
-                
-                blanks -= 1
-            print(guessList)
-            print(HANGMANPICS[lives])
-            print(f"*********{6 -lives} LIVES LEFT******0")
+        global lives
+        if let not in answerList:
+            lives += 1
+            print(f"You guessed {guessLetter}. That's not in the word. YOU LOSE A LIFE.\n")
 
-        elif let   in answerList and  let  in guessList:
-            print(f"You already guess {let}")
-            print(guessList)
-            print(HANGMANPICS[lives])
-            print(f"*********{6 -lives} LIVES LEFT******1")
-
-        # elif let not in answerList:
-        #     lives += 1
-        #     print(f"You guessed {guessLetter}. That's not in the word. YOU LOSE A LIFE.\n")
-        #     print(guessList)
-        #     print(HANGMANPICS[lives])
-        #     print(f"*********{6 -lives} LIVES LEFT******2")
+        for i in range(len(answerList)):
+            if answerList[i] == let and guessList[i] != let:
+                guessList[i] = let
+    
+        letterFinder(let)            
+        printHangman()        
 
 
+def printHangman():
+    print(guessList)
+    print(HANGMANPICS[lives])
+    print(f"*********{6 -lives} LIVES LEFT******")
 
-while lives !=  6:
+def letterFinder(let):
+    if let not in LetterList:
+        LetterList.append(let) 
+    
+    else:
+        print(f"You've already guessed {let}")
+
+
+while lives != 6 :
         
     letterCheck(guessLetter)
-    if guessLetter == answerList and lives != 6:
+    if guessList == answerList and lives != 6:
         print("YOU WIN!!")
     else:
     
         print(guessList)
-        print(f"Word to guess: {"_ " * blanks}")
+        print(f"Word to guess: {guessList}")
         guessLetter = input("Guess a letter: ")
         letterCheck(guessLetter)
 
                 
 if lives == 6:
-        print(f"*********IT WAS {answerLetter} YOU LOSE!!!********")
+        print(f"*********IT WAS {answerList} YOU LOSE!!!********")
     
 
 
