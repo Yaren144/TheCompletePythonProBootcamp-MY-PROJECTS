b1a9b4de6a97dd3c914f5de9c605e802686214c5
Ya44 <yarendeveci477@gmail.com>
2025-07-12 22:25:14 +0300

    Version 3 (added Documentation

--- ceaser_cipher.py
+++ ceaser_cipher.py
@@ -1,48 +1,77 @@
 import string
 alphabet = list(string.ascii_lowercase)
 
-def normalize_shift(shift_amount):
-    return shift_amount % 26
+def normalize_shift(shift):
+    """
+    Normalize shift value to bw within 0-25 range.
 
-# encode
-def encrypt(message_list, shift_amount):
-    text_e = message_list
+    Args:
+        shift (int): The shift amount
+
+    Returns:
+        int:Normalised shift value
+    """
+    
+
+    
+    return shift % 26
+
+
+def encrypt(message, shift):
+    """
+    Encrypt given message.
+
+    Args:
+        message (str): the message to encrypt
+        shift (int): The shift amount
+    """
+    text_e = message
 
     for i in range(len(text_e)):
         if text_e[i] not in alphabet:
             pass
         else:
             letter = text_e[i]
-            new_ind = normalize_shift(alphabet.index(letter) + shift_amount)
+            new_ind = normalize_shift(alphabet.index(letter) + shift)
             text_e[i] = alphabet[new_ind]
     print(f"Encoded message:{"".join(text_e)}")
          
 
-# decode
-def decrypt(message_list, shift_amount):
-    text_d = message_list
+
+def decrypt(message, shift):
+    """
+    Decrypt given message.
+
+    Args:
+        message (str): The message to process
+        shift (int): shift amount
+    """
+    text_d = message
 
     for i in range(len(text_d)):
         if text_d[i] not in alphabet:
             pass
         else:
             letter = text_d[i]
-            new_ind = normalize_shift(alphabet.index(letter) - shift_amount)
+            new_ind = normalize_shift(alphabet.index(letter) - shift)
             text_d[i] = alphabet[new_ind]
     print(f"Decoded message:{"".join(text_d)}")
 
 def main():
+    """
+    Main function to run the Ceasar cipher program.
+    """
     while True:
         code_type= input("Type encode to 'encrypt'. Type decode to 'decrypt':")
         original_text = input("Type your message: ").lower()
-        shift_amount = int(input("Type shift amount: "))
+        shift = int(input("Type shift amount: "))
         origin_list = list(original_text)
 
         if code_type == "encode":
-            encrypt(origin_list,shift_amount)
+            encrypt(origin_list,shift)
             
         elif code_type == "decode":
-            decrypt(origin_list, shift_amount)
+            decrypt(origin_list, shift)
 
         game_on = input(
             "Do you want to continue the game? Type: 'Yes' or 'No'"
